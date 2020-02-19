66e17d233a68614c2725af3840bd4d6f3fa02cf4
fredericmillard <fmillard@octo.com>
2020-02-19 16:54:08 +0100

    Prepare dojo 20022020

--- app/src/main/java/com/frederic/millard/android/kata/gameoflife/core/LivingCellAroundCalculator.kt
+++ app/src/main/java/com/frederic/millard/android/kata/gameoflife/core/LivingCellAroundCalculator.kt
@@ -3,19 +3,6 @@ package com.frederic.millard.android.kata.gameoflife.core
 class LivingCellAroundCalculator {
 
     fun computeLivingCellsAround(world: World, row: Int, col: Int): Int {
-        return with(world) {
-            listOf(
-                row - 1 to col - 1,
-                row - 1 to col,
-                row - 1 to col + 1,
-                row to col - 1,
-                row to col + 1,
-                row + 1 to col - 1,
-                row + 1 to col,
-                row + 1 to col + 1)
-                    .filter { it.first in 0 until height && it.second in 0 until width }
-                    .map { cells[it.first][it.second] }
-                    .count { it }
-        }
+        return TODO("Implem 1")
     }
 }
--- app/src/main/java/com/frederic/millard/android/kata/gameoflife/core/NextGenerationCalculator.kt
+++ app/src/main/java/com/frederic/millard/android/kata/gameoflife/core/NextGenerationCalculator.kt
@@ -2,18 +2,10 @@ package com.frederic.millard.android.kata.gameoflife.core
 
 class NextGenerationCalculator(private var livingCellAroundCalculator: LivingCellAroundCalculator) {
 
-    companion object {
-        private val AROUND_LIVING_CELL_COUNT_TO_CONTINUE_LIVING = listOf(2, 3)
-        private val AROUND_LIVING_CELL_COUNT_TO_BE_BORN = listOf(3)
-    }
-
     fun computeNextGeneration(world: World): World {
         val newCells = List(world.height) { row ->
             List(world.width) { col ->
-                val livingCellAround = livingCellAroundCalculator.computeLivingCellsAround(world, row, col)
-                val cell = world.cells[row][col]
-                if (cell) AROUND_LIVING_CELL_COUNT_TO_CONTINUE_LIVING.contains(livingCellAround)
-                else AROUND_LIVING_CELL_COUNT_TO_BE_BORN.contains(livingCellAround)
+                TODO("Implem 2")
             }
         }
         return world.copy(
